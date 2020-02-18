cc3a1a543927ad940d9fbf84cbe63a08a572f7ff
Lukas Nicholas Tveiten <lukasnt@stud.ntnu.no>
2020-02-18 08:32:11 +0100

    Apply suggestion to backend/leads/api/views.py

--- backend/leads/api/views.py
+++ backend/leads/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework import permissions
+from rest_framework.permissions import AllowAny
 
 from leads.models import User, Advert
 from .serializers import UserSerializer, AdvertSerializer
