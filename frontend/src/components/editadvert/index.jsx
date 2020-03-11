d8a47f77541d4881dfa01c785f04faec0ff30de7
Håvard Hjelmeseth <haavahje@stud.ntnu.no>
2020-03-11 15:15:17 +0100

    #5 - Bytte til å bruke axios for http req.

--- frontend/src/components/editadvert/index.jsx
+++ frontend/src/components/editadvert/index.jsx
@@ -5,121 +5,136 @@ import {
   Typography,
   Grid,
   TextField,
-  Button
+  Button,
+  Snackbar
 } from "@material-ui/core";
 import ImageIcon from "@material-ui/icons/Image";
+import axios from "axios";
+import { useState } from "react";
+import { Alert } from "@material-ui/lab";
 
 export default function EditAdvert({ accessToken }) {
+  const [openModal, setOpenModal] = useState(false);
+  const [modalText, setModalText] = useState("");
   return (
-    <Container maxWidth="sm" className="edit-advert">
-      <Grid container direction="column" spacing={2}>
-        <Grid item>
-          <Typography variant="h3">Rediger annonse</Typography>
-        </Grid>
-        <Grid item>
-          <TextField
-            autoComplete="off"
-            id="title"
-            fullWidth
-            variant="outlined"
-            label="Tittel"
-          ></TextField>
-        </Grid>
-        <Grid item>
-          <TextField
-            fullWidth
-            autoComplete="off"
-            id="price"
-            variant="outlined"
-            label="Pris i NOK"
-          ></TextField>
-        </Grid>
-        <Grid item>
-          <TextField
-            fullWidth
-            multiline
-            autoComplete="off"
-            id="description"
-            rows="8"
-            rowsMax="16"
-            variant="outlined"
-            label="Beskrivelse av salgsgjenstanden"
-          ></TextField>
-        </Grid>
-        <Grid item>
-          <input
-            className="img-upload"
-            accept="image/*"
-            id="contained-button-file"
-            multiple
-            type="file"
-          />
-          <label htmlFor="contained-button-file">
+    <div>
+      <Container maxWidth="sm" className="edit-advert">
+        <Grid container direction="column" spacing={2}>
+          <Grid item>
+            <Typography variant="h3">Rediger annonse</Typography>
+          </Grid>
+          <Grid item>
+            <TextField
+              autoComplete="off"
+              id="title"
+              fullWidth
+              variant="outlined"
+              label="Tittel"
+            ></TextField>
+          </Grid>
+          <Grid item>
+            <TextField
+              fullWidth
+              autoComplete="off"
+              id="price"
+              variant="outlined"
+              label="Pris i NOK"
+            ></TextField>
+          </Grid>
+          <Grid item>
+            <TextField
+              fullWidth
+              multiline
+              autoComplete="off"
+              id="description"
+              rows="8"
+              rowsMax="16"
+              variant="outlined"
+              label="Beskrivelse av salgsgjenstanden"
+            ></TextField>
+          </Grid>
+          <Grid item>
+            <input
+              className="img-upload"
+              accept="image/*"
+              id="contained-button-file"
+              multiple
+              type="file"
+            />
+            <label htmlFor="contained-button-file">
+              <Button
+                fullWidth
+                variant="contained"
+                component="span"
+                startIcon={<ImageIcon />}
+              >
+                Last opp bilde
+              </Button>
+            </label>
+          </Grid>
+          <Grid item>
             <Button
               fullWidth
               variant="contained"
-              component="span"
-              startIcon={<ImageIcon />}
+              color="primary"
+              size="large"
+              onClick={() => {
+                UpdateAdvert(accessToken, setOpenModal, setModalText);
+              }}
             >
-              Last opp bilde
+              Lagre annonse
             </Button>
-          </label>
+          </Grid>
         </Grid>
-        <Grid item>
-          <Button
-            fullWidth
-            variant="contained"
-            color="primary"
-            size="large"
-            onClick={() => {
-              UpdateAdvert(accessToken);
-            }}
-          >
-            Lagre annonse
-          </Button>
-        </Grid>
-      </Grid>
-    </Container>
+      </Container>
+      <Snackbar
+        open={openModal}
+        autoHideDuration={4000}
+        onClose={() => setOpenModal(false)}
+      >
+        <Alert onClose={() => setOpenModal(false)} severity="error">
+          {modalText}
+        </Alert>
+      </Snackbar>
+    </div>
   );
 }
 
-function UpdateAdvert(accessToken) {
+function UpdateAdvert(accessToken, setOpenModal, setModalText) {
   const title = document.getElementById("title").value;
   const price = document.getElementById("price").value;
   const description = document.getElementById("description").value;
   const image = document.querySelector('input[type="file"]');
   const url = "http://127.0.0.1:8000/api/marketplace/saleItems/";
-  var error = false;
-  console.log("Token: " + accessToken);
 
-  let form_data = new FormData();
-  form_data.append("image", image.files[0]);
+  const form_data = new FormData();
   form_data.append("title", title);
-  form_data.append("price", price);
+  form_data.append("creator", 1);
   form_data.append("description", description);
+  form_data.append("img", image.files[0]);
+  form_data.append("price", price);
 
-  fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type":
-        "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
-      Authorization: "Bearer" + " " + accessToken
-    },
-    body: form_data
-  })
-    .then(res => {
-      if (res.status >= 400) {
-        console.log("Error");
-        error = true;
+  axios
+    .post(url, form_data, {
+      headers: {
+        Authorization: "Bearer" + " " + accessToken,
+        "content-type":
+          "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW"
       }
-      return res.json();
     })
     .then(res => {
-      if (!error) {
-        console.log("Success!");
-        console.log(res);
+      console.log(res.data);
+      console.log("Success!");
+    })
+    .catch(err => {
+      if (err.response.status === 401) {
+        setModalText("Din tilgangstoken har utløpt...");
+      } else if (err.response.status === 400) {
+        setModalText("Alle felter må fylles ut!");
       } else {
-        console.log(res);
+        setModalText(err.response.status + ": " + err.response.statusText);
       }
+      console.log(err);
+      setOpenModal(true);
     });
 }
